48dac973f07987fe00b9cfe5c59ddaff9f40d0d8
ismetozalp <ismet@hive.com.tr>
2026-04-29 23:49:07 +0300

    added ZoneId handling in ZoneIdAsStringCodec and created corresponding tests

--- src/test/java/tr/com/hive/smm/codes/time/ZoneIdAsStringCodecTest.java
+++ src/test/java/tr/com/hive/smm/codes/time/ZoneIdAsStringCodecTest.java
@@ -1,25 +1,83 @@
 package tr.com.hive.smm.codes.time;
 
 import org.bson.BsonDocument;
+import org.bson.BsonDocumentReader;
 import org.bson.BsonDocumentWriter;
+import org.bson.BsonString;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.EncoderContext;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 import tr.com.hive.smm.codecs.time.ZoneIdAsStringCodec;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class ZoneIdAsStringCodecTest {
   @Test
-  void test() {
-    ZoneId zoneId = ZoneId.of("Europe/Istanbul");
+  void encode_RegionBasedZoneId_WritesNormalizedString() {
+
     ZoneIdAsStringCodec zoneIdAsStringCodec = new ZoneIdAsStringCodec();
-    BsonDocumentWriter writer = new BsonDocumentWriter(new BsonDocument());
+
+    BsonDocument document = new BsonDocument();
+    BsonDocumentWriter writer = new BsonDocumentWriter(document);
+
+    ZoneId zoneId = ZoneId.of("Europe/Berlin");
+
     writer.writeStartDocument();
-    writer.writeName("zoneId");
-    zoneIdAsStringCodec.encode(writer, zoneId, null);
-//    BsonDocument zoneIdValue = new BsonDocument("zoneId", new BsonString("Europe/Istanbul"));
-//    BsonDocumentReader reader = new BsonDocumentReader(zoneIdValue);
-//    reader.readBsonType("zoneId");
-//    zoneIdAsStringCodec.decode(reader, null);
+    writer.writeName("zone");
+    zoneIdAsStringCodec.encode(writer, zoneId, EncoderContext.builder().build());
+    writer.writeEndDocument();
+
+    assertTrue(document.containsKey("zone"));
+    assertEquals("Europe/Berlin", document.getString("zone").getValue());
+  }
+
+  @Test
+  void encode_OffsetBasedZoneId_WritesIdString() {
+    ZoneIdAsStringCodec zoneIdAsStringCodec = new ZoneIdAsStringCodec();
+
+    BsonDocument document = new BsonDocument();
+    BsonDocumentWriter writer = new BsonDocumentWriter(document);
+
+    ZoneId zoneOffset = ZoneOffset.of("+05:30");
+
+    writer.writeStartDocument();
+    writer.writeName("offset");
+    zoneIdAsStringCodec.encode(writer, zoneOffset, EncoderContext.builder().build());
+    writer.writeEndDocument();
+
+    assertEquals("+05:30", document.getString("offset").getValue());
+  }
+
+  @Test
+  void decode_RealBsonString_ReturnsZoneId() {
+    ZoneIdAsStringCodec zoneIdAsStringCodec = new ZoneIdAsStringCodec();
+
+    BsonDocument doc = new BsonDocument("id", new BsonString("Europe/London"));
+    BsonDocumentReader reader = new BsonDocumentReader(doc);
+
+    reader.readStartDocument();
+    reader.readName("id");
+
+    ZoneId result = zoneIdAsStringCodec.decode(reader, DecoderContext.builder().build());
+    reader.readEndDocument();
+
+    assertEquals(ZoneId.of("Europe/London"), result);
+  }
+
+  @Test
+  void decode_RealOffsetString_ReturnsZoneOffset() {
+    ZoneIdAsStringCodec zoneIdAsStringCodec = new ZoneIdAsStringCodec();
+    BsonDocument doc = new BsonDocument("id", new BsonString("-05:00"));
+    BsonDocumentReader reader = new BsonDocumentReader(doc);
+    reader.readStartDocument();
+    reader.readName("id");
+    ZoneId result = zoneIdAsStringCodec.decode(reader, DecoderContext.builder().build());
+    reader.readEndDocument();
+    assertEquals(ZoneOffset.of("-05:00"), result);
   }
 }
